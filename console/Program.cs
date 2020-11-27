bbe5669b969b0b7ac3ae66da2b722976cd15b37c
Poveda <julianopoveda@github.com>
2020-11-27 08:18:27 -0300

    simple if

---
+++ console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tdc_ifless
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args[0]=="hello")
+                Console.WriteLine("Hello World!");
+            else            
+                Console.WriteLine("See you");   
+        }
+    }
+}
